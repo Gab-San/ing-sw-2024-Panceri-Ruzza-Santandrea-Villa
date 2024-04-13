f436406cb340152f14f23512a48273c1cb0dd63e
Alessandro_Ruzza <alessandroruzza22@gmail.com>
2024-04-13 20:30:05 +0200

    Changed filled corners to exist and use FILLED resource
    - Added GameResource.FILLED
    - Changed other Card classes to check for FILLED resource instead of null corner
    - Changed PlayArea accordingly
    - Fixed StartingCard to always use corners (front/back)

--- src/main/java/it/polimi/ingsw/model/cards/StartingCard.java
+++ src/main/java/it/polimi/ingsw/model/cards/StartingCard.java
@@ -3,6 +3,7 @@ package it.polimi.ingsw.model.cards;
 import it.polimi.ingsw.model.Point;
 import it.polimi.ingsw.model.enums.*;
 
+import java.security.InvalidParameterException;
 import java.util.*;
 import java.util.Set;
 
@@ -11,36 +12,25 @@ import java.util.Set;
  */
 public class StartingCard extends PlaceableCard {
     private final List<GameResource> centralFrontResources;
-    private final Hashtable<CornerDirection, GameResource> frontCornersResources;
 
     /**
      * Constructs a blank card.
      */
-    public StartingCard(){
+    public StartingCard() {
         super();
-        this.centralFrontResources = null;
-        this.frontCornersResources = null;
+        this.centralFrontResources = new ArrayList<>();
     }
 
     /**
      * Constructs a starting card filled with the required information.
-     * @param frontCornRes The possible front corners' resources
      * @param centralRes The resources shown in the center of the card
      * @param corners The corners of the card
      */
-    //FIXME: how to set a filled frontCorner??
-    public StartingCard(Hashtable<CornerDirection, GameResource> frontCornRes,
-                        GameResource[] centralRes, Corner... corners){
+    public StartingCard(GameResource[] centralRes, Corner... corners) throws InvalidParameterException {
         super(corners);
 
         this.centralFrontResources = new ArrayList<>();
-        //TODO: Check if this copies;
         centralFrontResources.addAll(Arrays.asList(centralRes));
-
-        this.frontCornersResources = new Hashtable<>();
-        for(CornerDirection cornDir: frontCornRes.keySet()){
-            this.frontCornersResources.put(cornDir, frontCornRes.get(cornDir));
-        }
     }
 
     /**
@@ -51,44 +41,18 @@ public class StartingCard extends PlaceableCard {
      private StartingCard(Point placement, StartingCard oldCard) {
         super(placement, oldCard);
         this.centralFrontResources = oldCard.centralFrontResources;
-        this.frontCornersResources = oldCard.frontCornersResources;
     }
 
     @Override
     public Map<GameResource, Integer> getCardResources() {
-        int[] resourcesCount = new int[7];
-
-        if(!isFaceUp) {
-            Set<CornerDirection> cornerKeys = corners.keySet();
-
-            for (CornerDirection cornDir : cornerKeys) {
-                GameResource res = corners.get(cornDir).getResource();
-                if (res != null) {
-                    resourcesCount[res.getResourceIndex()]++;
-                }
-            }
-
-        } else{
-
-            Set<CornerDirection> cornerKeys = frontCornersResources.keySet();
-            for(CornerDirection cornDir: cornerKeys){
-                GameResource res = corners.get(cornDir).getResource();
-                if(res != null){
-                    resourcesCount[res.getResourceIndex()]++;
-                }
-            }
-
+        int[] resourcesCount = super.getCornerResources();
+        if(isFaceUp) {
             for(GameResource res: centralFrontResources){
                 resourcesCount[res.getResourceIndex()]++;
             }
         }
 
-        Hashtable<GameResource, Integer> countedResources = new Hashtable<>();
-        for (GameResource r : GameResource.values()){
-            countedResources.put(r, resourcesCount[r.getResourceIndex()]);
-        }
-
-        return countedResources;
+        return super.resourceArrayToMap(resourcesCount);
     }
 
     @Override
--- src/main/java/it/polimi/ingsw/model/enums/GameResource.java
+++ src/main/java/it/polimi/ingsw/model/enums/GameResource.java
@@ -2,7 +2,8 @@ package it.polimi.ingsw.model.enums;
 
 public enum GameResource {
     MUSHROOM(1), BUTTERFLY(2),LEAF(3),
-    WOLF(4), SCROLL(5),POTION(6),QUILL(7);
+    WOLF(4), SCROLL(5),POTION(6),QUILL(7),
+    FILLED(-1);
     private final int resourceIndex;
     private GameResource(int resourceIndex){
         this.resourceIndex = resourceIndex;
