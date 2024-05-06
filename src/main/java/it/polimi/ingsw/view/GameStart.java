c716eff2b1ce9d4ed421a3fef88a36e5bc002c3a
Maddalena Panceri <160673509+maddalenapanceri@users.noreply.github.com>
2024-05-06 17:25:28 +0200

    TUI

--- src/main/java/it/polimi/ingsw/view/GameStart.java
+++ src/main/java/it/polimi/ingsw/view/GameStart.java
@@ -7,14 +7,13 @@ public class GameStart {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Chiedi all'utente la modalità di gioco
         System.out.println("Enter the game mode (TUI o GUI):");
         String gameMode = scanner.nextLine();
 
         if (gameMode.equalsIgnoreCase("GUI")) {
-            GUIStarter.startGUI(); // Avvia la schermata grafica
+            GUIStarter.startGUI();
         } else if (gameMode.equalsIgnoreCase("TUI")) {
-            TUIStarter.startTUI();// Avvia la schermata testuale
+            TUIStarter.startTUI();
         } else {
             System.out.println("Modalità di gioco non valida.");
         }
--- src/main/java/it/polimi/ingsw/view/TUIStarter.java
+++ src/main/java/it/polimi/ingsw/view/TUIStarter.java
@@ -8,19 +8,15 @@ public class TUIStarter {
         Scanner scanner = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out, true);
 
-        // Chiedi all'utente il tipo di connessione
         out.println("Enter connection type (TCP o RMI):");
         String connectionType = scanner.nextLine();
 
-        // Chiedi all'utente di scegliere un nickname
         out.println("Choose your nickname:");
         String nickname = scanner.nextLine();
 
-        // Chiedi all'utente di inserire l'ID della carta da disegnare
         out.println("Enter card ID (e.g., G0, G33):");
         String cardId = scanner.nextLine();
 
-        // Crea un oggetto GoldCardJSON per la carta specificata
         GoldCardJSON card = retrieveCardFromModel(cardId);
 
         // Disegna la carta specificata
@@ -30,10 +26,7 @@ public class TUIStarter {
         scanner.close();
     }
 
-    // Metodo per recuperare un'istanza di GoldCardJSON dal modello dei dati
     private static GoldCardJSON retrieveCardFromModel(String cardId) {
-        // Implementa la logica per recuperare l'oggetto GoldCardJSON dal tuo modello dei dati
-        // Restituisce un'istanza fittizia in questo esempio
         GoldCardJSON card = new GoldCardJSON();
         card.setCardId(cardId);
         return card;
