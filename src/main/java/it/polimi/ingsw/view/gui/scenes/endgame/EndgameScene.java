d906ac46ec2747b40e910b82c1554abb9bf90f60
Alessandro_Ruzza <alessandroruzza22@gmail.com>
2024-06-21 12:07:37 +0200

    Added final scores to EndgameScene

--- src/main/java/it/polimi/ingsw/view/gui/scenes/endgame/EndgameScene.java
+++ src/main/java/it/polimi/ingsw/view/gui/scenes/endgame/EndgameScene.java
@@ -19,13 +19,16 @@ import java.util.List;
 
 public class EndgameScene extends JPanel implements GUI_Scene, ActionListener {
     private final GameInputHandler inputHandler;
+    private final ViewBoard board;
     private final ScoreboardPanel scoreboard;
     private final boolean atLeast2Players;
     protected Timer displayTimer;
     private final JLabel notificationLabel;
+    private static final int STANDARD_WINNER_TEXT_FONT = 50;
 
     public EndgameScene(ViewBoard board, GameInputHandler inputHandler){
         this.inputHandler = inputHandler;
+        this.board = board;
         setLayout(new BorderLayout());
 
         JPanel winnerPanel = setupWinnerPanel();
@@ -42,18 +45,18 @@ public class EndgameScene extends JPanel implements GUI_Scene, ActionListener {
         JLabel winnerTextLabel;
         JLabel winnerNickLabel;
         if(winnerNick.equals(board.getPlayerHand().getNickname())) {
-            winnerTextLabel = setupWinnerLabel("Congratulations!", null);
+            winnerTextLabel = setupLabel("Congratulations!", null);
 
             String nick = "You won";
             if(!atLeast2Players)
                 nick += " BY DEFAULT.";
             else nick += ".";
 
-            winnerNickLabel = setupWinnerLabel(nick, board.getPlayerHand().getColor());
+            winnerNickLabel = setupLabel(nick, board.getPlayerHand().getColor());
         }
         else {
-            winnerTextLabel = setupWinnerLabel("The winner is: ", null);
-            winnerNickLabel = setupWinnerLabel(winnerNick, board.getOpponentHand(winnerNick).getColor());
+            winnerTextLabel = setupLabel("The winner is: ", null);
+            winnerNickLabel = setupLabel(winnerNick, board.getOpponentHand(winnerNick).getColor());
         }
 
         winnerPanel.add(winnerTextLabel,
@@ -91,23 +94,49 @@ public class EndgameScene extends JPanel implements GUI_Scene, ActionListener {
                 ViewCard.getScaledHeight() * 3 + 4 * 10 + 25));
         panel.setBackground(GameColor.BOARD_COLOUR.getColor());
 
+    //region Add_All_Players_Score
+        int gridY = 1;
+        List<ViewHand> handsByScore = board.getAllPlayerHands().stream()
+                .sorted((h1,h2) -> -Integer.compare(board.getScore(h1.getNickname()), board.getScore(h2.getNickname())))
+                .toList();
+        int scoreFontSize = 35;
+        for(ViewHand hand : handsByScore){
+            JLabel nickLabel = setupLabel(hand.getNickname(), hand.getColor(), scoreFontSize);
+            String scoreStr = Integer.toString(board.getScore(hand.getNickname()));
+            JLabel scoreLabel = setupLabel(scoreStr, null, scoreFontSize);
+
+            panel.add(nickLabel,
+                    new GridBagConstraints(0, gridY,2 ,1 ,0.5 ,0.5,
+                            GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(10,10,10,10), 0,0));
+            panel.add(scoreLabel,
+                    new GridBagConstraints(2, gridY ,1 ,1 ,0.5 ,0.5,
+                            GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(10,10,10,10), 0,0));
+            gridY++;
+        }
+    //endregion
+
+    //region Add_Restart_Buttons
         for (int i = 2; i <= 4; i++) {
             JButton restartButton = new JButton("RESTART "+i);
             restartButton.setFocusable(false);
             restartButton.addActionListener(this);
 
             int gridX = i-2;
-            int gridY = 1;
+            //gridY is as updated in the previous scoreLabel additions
             panel.add(restartButton,
-                    new GridBagConstraints(gridX,gridY ,1 ,1 ,0.5 ,0.5,
+                    new GridBagConstraints(gridX, gridY ,1 ,1 ,0.5 ,0.5,
                     GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(10,10,10,10), 0,0));
         }
+    //endregion
         return panel;
     }
 
-    private JLabel setupWinnerLabel(String text, PlayerColor color){
+    private JLabel setupLabel(String text, PlayerColor color){
+        return setupLabel(text, color, STANDARD_WINNER_TEXT_FONT);
+    }
+    private JLabel setupLabel(String text, PlayerColor color, int size){
         JLabel label = new JLabel(text);
-        label.setFont(new Font("Raleway", Font.PLAIN, 30));
+        label.setFont(new Font("Raleway", Font.PLAIN, size));
         label.setForeground(PlayerColor.getColor(color));
         label.setAlignmentX(CENTER_ALIGNMENT);
         label.setVisible(true);
@@ -169,7 +198,9 @@ public class EndgameScene extends JPanel implements GUI_Scene, ActionListener {
     public void actionPerformed(ActionEvent e) {
         JButton button = (JButton) e.getSource();
         int restartNum = Integer.parseInt(button.getText().split(" ")[1]);
-        if(restartNum >= 2 && restartNum <= 4)
-            inputHandler.restartGame(restartNum);
+//        if(restartNum >= 2 && restartNum <= 4)
+//            inputHandler.restartGame(restartNum);
+
+        //TODO: restart GUI and send restartGame command
     }
 }
